aca21d6c542168d8dae9aa19f3ac54569ea8c688
tlrruddbs <tlrruddbs@gmail.com>
2022-09-08 14:10:19 +0900

    sikproject: 빈 조회

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,11 +1,9 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
-import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServieImpl;
+import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
@@ -16,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
     @Bean
     public MemberService memberService(){
-        return new MemberServieImpl(memberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
     @Bean
     public static MemoryMemberRepository memberRepository() {
--- core/src/test/java/hello/core/beanfind/ApplicationContextTest.java
+++ core/src/test/java/hello/core/beanfind/ApplicationContextTest.java
@@ -3,6 +3,7 @@ package hello.core.beanfind;
 import hello.core.AppConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ApplicationContextTest {
@@ -15,6 +16,17 @@ public class ApplicationContextTest {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
             System.out.println("beanDefinitionName = " + beanDefinitionName);
+        }
+    }
+    @Test
+    @DisplayName("모든 bean 출력하기")
+    void findApplicationBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
+                System.out.println("beanDefinition = " + beanDefinitionName);
+            }
         }
     }
 
